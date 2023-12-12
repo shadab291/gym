a2a2af7930bb80a817a63ec9ef82deb69286b6ad
shadab291 <70841658+shadab291@users.noreply.github.com>
2023-12-12 11:04:23 +0530

    Update UserService.js

--- src/Component/Services/UserService.js
+++ src/Component/Services/UserService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const USER_API_BASE_URL = "http://localhost:8080/api/v1/users";
+const USER_API_BASE_URL = "http://localhost:8082/api/v1/users";
 
 
 class UserService {
