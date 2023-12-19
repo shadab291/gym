277988ca0f636f40b6faf5de677eaf846ef6d75a
shadab291 <70841658+shadab291@users.noreply.github.com>
2023-12-19 11:52:58 +0530

    Add files via upload

--- src/Component/Assets/Home.js
+++ src/Component/Assets/Home.js
@@ -4,15 +4,13 @@ import "tailwindcss/tailwind.css";
 import machine1 from "./Assets_Logo/machine1.png";
 import EnquiryForm from "../CommonComponents/EnquiryForm";
 
-
 const Home = () => {
   return (
-    
     <div className="bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 min-h-screen">
       <header className="bg-cover bg-center bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-white py-16">
         <Container>
           <Typography variant="h2" component="h1" gutterBottom>
-            <span className="inline-flex h-20 pt-2 overflow-x-hidden animate-type group-hover:animate-type-reverse whitespace-nowrap text-brand-accent will-change-transform">Welcome to Gymbulls</span>
+          <marquee behavior="" >Welcome to Gymbull</marquee>
           </Typography>
           <Typography variant="h5" gutterBottom>
             Your Fitness Journey Starts Here
@@ -25,29 +23,18 @@ const Home = () => {
       <section className="py-16 text-white">
         <Container className="">
           <Grid container spacing={3}>
-            <Grid item xs={40}>
+            <Grid item xs={12} md={6} className="md:flex md:justify-center">
               <img
                 src={machine1}
                 alt="machine"
-                align="left"
-                className="transition-transform ease-in-out hover:scale-105 animate-fade-down"
+                className="transition-transform ease-in-out hover:scale-105 animate-fade-down w-full md:w-auto"
               />
-              <Typography
-                variant="h4"
-                component="h2"
-                align="right"
-                className="pt-20 text-white"
-                gutterBottom
-              >
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Typography variant="h4" component="h2" gutterBottom>
                 Join Our Community
               </Typography>
-              <Typography
-                variant="body1"
-                className="text-left
-"
-                align="right"
-                paragraph
-              >
+              <Typography variant="body1" paragraph>
                 Explore our extensive range of workout equipment designed to
                 help you reach your fitness goals.
               </Typography>
@@ -58,16 +45,11 @@ const Home = () => {
       <section className="py-16 pt-10 text-white">
         <Container className="">
           <Grid container spacing={3}>
-            <Grid item xs={40}>
-              <Typography
-                variant="h4"
-                component="h2"
-                align="center"
-                gutterBottom
-              >
+            <Grid item xs={12}>
+              <Typography variant="h4" component="h2" gutterBottom>
                 Our Commitment
               </Typography>
-              <Typography variant="body1" align="left" paragraph>
+              <Typography variant="body1" paragraph>
                 Our commitment to our customers is simple: to offer a curated
                 selection of the finest workout equipment, backed by
                 unparalleled customer support. We know that choosing the right
